9fc2e90004b8228fb6009fb4c55d6548b5e79a56
MaximilianAwiti <maxawiti@gmail.com>
2025-02-19 11:33:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function closeModal() {
     document.getElementById("imageModal").style.display = "none";
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("visible");
+            navbar.classList.remove("hidden");
+        } else {
+            navbar.classList.add("hidden");
+            navbar.classList.remove("visible");
+        }
+    });
+});
